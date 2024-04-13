63e391d0bb9787eed373280688c47535563b98d7
Guanghui Liang <meetguanghuiliang@gmail.com>
2024-04-13 16:53:12 +0800

    Initial Commit

---
+++ SwiftUISearchBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUISearchBar
+//
+//  Created by 梁光辉 on 2024/4/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUISearchBar/SwiftUISearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISearchBarApp.swift
+//  SwiftUISearchBar
+//
+//  Created by 梁光辉 on 2024/4/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
